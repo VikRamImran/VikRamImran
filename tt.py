846775a6537dfca07e517d0d58af16903d0ac770
VIKRAM <jayaramrose95@gmail.com>
2023-07-21 05:37:43 +0530

    test file

---
+++ tt.py
@@ -0,0 +1,12 @@
+import re
+import os
+import io
+import sys
+import json
+import numpy as np
+import pandas as pd
+    
+#dfs = pd.read_html(str(sys.argv[1]))
+a=os.path.join(sys.argv[1],sys.argv[2])
+print(a)
+
